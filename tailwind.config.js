7832ee73ed30ec1c39db41378b60f0f2958e1bf6
Muhammad Rafif <radif.adityo6@gmail.com>
2023-07-14 13:21:41 +0700

    Add Card

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {},
+  theme: { 
+    extend: {
+      colors: {
+        'dark-blue': '#172554'
+      }
+    },
   },
   plugins: [],
 }
